8f49b224ce2463f2eac431bea8c1c3f2c203eb09
Abhishek <51646455+itsamittal@users.noreply.github.com>
2025-07-31 12:15:05 +0530

    Update Assignment 4.py

--- Assignment 4.py
+++ Assignment 4.py
@@ -1,3 +1,4 @@
+Module 5: Files, Exceptions, and Errors in Python
 
 # coding: utf-8
 
@@ -18,3 +19,31 @@ def read_file(filename):
 # Call the function with 'sample.txt'
 read_file('sample.txt')
 
+
+
+
+def write_to_file(filename):
+    user_input = input("Enter text to write to the file: ")
+    with open(filename, 'w') as file:
+        file.write(user_input + '\n')
+    print("Data Successfully written to output.txt.")
+
+def append_to_file(filename):
+    more_input = input("Enter additional text to append.: ")
+    with open(filename, 'a') as file:
+        file.write(more_input + '\n')
+    print("Additional data appended to file.")
+
+def read_file(filename):
+    print("\nFinal content of output.txt.:")
+    with open(filename, 'r') as file:
+        for line in file:
+            print(line, end='')
+
+# File name
+filename = 'output.txt'
+
+# Perform operations
+write_to_file(filename)
+append_to_file(filename)
+read_file(filename)
